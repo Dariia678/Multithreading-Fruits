4026610d2110c2b8f070899b4f27fe57dc2ecc9d
Makarenko Dariia <vermicell.i678@gmail.com>
2024-07-13 12:55:25 +0200

    Initial commit

---
+++ src/app/MyThread.java
@@ -0,0 +1,19 @@
+package app;
+
+public class MyThread extends Thread{
+
+    private final DataHandler dataHandler;
+
+    public MyThread(String name, DataHandler dataHandler) {
+        super(name);
+        this.dataHandler = dataHandler;
+    }
+
+    @Override
+    public void run() {
+        dataHandler.getOutput();
+    }
+
+    public void start() {
+    }
+}
